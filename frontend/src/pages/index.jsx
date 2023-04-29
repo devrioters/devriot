9a6f22f67535a635ad5040b6cc4c20b7b1e91361
mathesukkj <kemuel.g7363@gmail.com>
2023-04-28 22:56:36 -0300

    init: frontend

---
+++ frontend/src/pages/index.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <>
+            <Image src="./next.svg" alt="vsf next" width={120} height={39} />
+            <h3>roger eh gay</h3>
+        </>
+    );
+}
